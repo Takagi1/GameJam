40aea9b68891e5e6db701e897531c3ed475b610a
Takagi1 <Retepa24@gmail.com>
2021-01-30 12:50:16 -0500

    Timer

--- Source/GameJam/GameJamGameMode.cpp
+++ Source/GameJam/GameJamGameMode.cpp
@@ -12,8 +12,8 @@ AGameJamGameMode::AGameJamGameMode()
 	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/CustomContent/Character"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
-	static ConstructorHelpers::FClassFinder<APlayerState> PlayerStateClassFinder(TEXT("/Game/CustomContent/PlayerState"));
-	PlayerStateClass = PlayerStateClassFinder.Class;
+	//static ConstructorHelpers::FClassFinder<APlayerState> PlayerStateClassFinder(TEXT("/Game/CustomContent/PlayerState"));
+	//PlayerStateClass = PlayerStateClassFinder.Class;
 
 	// use our custom HUD class
 	HUDClass = AGameJamHUD::StaticClass();
